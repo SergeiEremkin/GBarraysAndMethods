b6bfba895b0ac92b547d88ea2078b26c86b11e7d
SergeiEremkin <ffr@list.ru>
2022-06-07 20:06:57 +0300

    add 3 methods FillArray, PrintArray, FindElementByIndex

--- RandomArray/Program.cs
+++ RandomArray/Program.cs
@@ -1,2 +1,44 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int[] array = new int[10];
+
+void FillArray(int[] arr)
+{
+    int lenght = arr.Length;
+    int index = 0;
+
+    while (index < lenght)
+    {
+        arr[index] = new Random().Next(1, 10);
+
+        index++;
+    }
+}
+
+void PrintArray(int[] array)
+{
+    int lenght = array.Length;
+    int index = 0;
+    while (index < lenght)
+    {
+        Console.WriteLine(array[index]);
+        index++;
+    }
+}
+int FindElementByIndex(int[] array, int index)
+{
+    int lenght = array.Length;
+    int element = 0;
+    while (element < lenght)
+    {
+        if (array[element] == index)
+        {
+            element++;
+            break;
+        }
+    }
+    return array[element];
+}
+
+FillArray(array);
+PrintArray(array);
+FindElementByIndex(array, 3);
+
